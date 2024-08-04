1e74da987434bb1d6193c46ada1c33af24e71df0
Andy Wardley <abw@wardley.org>
2024-08-04 10:39:19 +0100

    External links

--- web/CardLink.jsx
+++ web/CardLink.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const CardLink = ({ href, Picture }) =>
-  <a href={href} className="card surface hover pad-a-4 border bdr-2 shadow-1">
+  <a
+    href={href}
+    className="card surface hover pad-a-4 border bdr-2 shadow-1"
+    target="_blank" rel="noreferrer"
+  >
     <Picture/>
   </a>
 
--- web/Link.jsx
+++ web/Link.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Link = ({ url, text, children }) =>
   <div>
-    <a href={url} className="larger">{url}</a>
+    <a
+      href={url} className="larger"
+      target="_blank" rel="noreferrer"
+    >{url}</a>
     <p className="mar-t-none">
       {text || children}
     </p>
